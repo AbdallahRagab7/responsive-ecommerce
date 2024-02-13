95e0c0229d417376fb94737119bae4d2297a7136
Abdallah Ragab <a.ragab8882@gmail.com>
2024-02-13 12:21:29 +0200

    styling products

--- src/Components/Home/Products.jsx
+++ src/Components/Home/Products.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import classes from "./Products.module.css"
+import classes from "./Products.module.css";
 function Products() {
   const [products, setProducts] = useState([]);
   useEffect(() => {
@@ -13,36 +13,52 @@ function Products() {
 
   return (
     <>
-      <div className="buttons text-center py-5">
-        <button className="btn btn-outline-dark btn-sm m-3">All</button>
-        <button className="btn btn-outline-dark btn-sm m-3">
+      <h1 className="text-center "> Latest Products</h1>
+      <div className="buttons text-center py-3 mb-2">
+        <button className="btn btn-outline-dark btn-sm m-2">All</button>
+        <button className="btn btn-outline-dark btn-sm m-2">
           Men's Clothing
         </button>
-        <button className="btn btn-outline-dark btn-sm m-3">
+        <button className="btn btn-outline-dark btn-sm m-2">
           Women's Clothing
         </button>
-        <button className="btn btn-outline-dark btn-sm m-3">Jewelery</button>
-        <button className="btn btn-outline-dark btn-sm m-3">Electronics</button>
+        <button className="btn btn-outline-dark btn-sm m-2">Jewelery</button>
+        <button className="btn btn-outline-dark btn-sm m-2">Electronics</button>
       </div>
 
-      <div className=" container ">
-        <div className="row ">
+      <div className="">
+        <div className="row justify-content-center align-items-center g-5">
           {products.map((product) => {
+            const mergeTitle = product.title.split(" ");
+            const title = `${mergeTitle[0]}  ${mergeTitle[1]}  ${mergeTitle[2]}`;
+
+            const first20Words = product.description
+              .split(" ")
+              .slice(0, 10)
+              .join(" ");
+
             return (
-              <div key={product.id} className="col-4 mb-5">
-                <div className={classes['product-image']}>
-                  <img src={product.image} alt="product-image" className="image-fluid" />
+              <div
+                key={product.id}
+                className=" card text-center col-8 col-sm-5 col-md-3 mx-3 "
+              >
+                <div className={`${classes["product-image"]} m-3`}>
+                  <img src={product.image} alt="product-image" />
                 </div>
-                
-                <h4>{product.title.substring(0,20)}...</h4>
-                <p className="description">
-                  {product.description.substring(0, 60)}...
-                </p>
-                <div className="price">
-                  <span>$ {product.price}</span>
+
+                <div>
+                  <h4>{title}</h4>
+                  <p className={classes.description}>
+                    {/* {product.description.substring(0, 60)}... */}
+                    {first20Words}
+                  </p>
                 </div>
-                <hr />
-                <div className="d-flex justify-content-center align-items-center">
+
+                <ul className="list-group list-group-flush mb-2">
+                  <li className="list-group-item lead fw-bold">$ {product.price}</li>
+                </ul>
+
+                <div className="d-flex justify-content-center mb-2">
                   <a to={"/product/" + product.id} className="btn btn-dark m-1">
                     Buy Now
                   </a>
